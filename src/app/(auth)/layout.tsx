6f175acbb086ae2c615bc4ebe1f523904d66021b
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2023-09-20 01:29:24 +0330

    Implemented sign in and sign up functionality

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div className="h-full bg-indigo-600">{children}</div>;
+  return (
+    <div className="flex items-center justify-center h-full">{children}</div>
+  );
 }
 
 export default AuthLayout;
