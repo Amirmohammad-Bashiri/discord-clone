7622ec147157206072c9c7ca92f02d835fa803bc
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2023-09-26 15:17:17 +0330

    Implemented edit and delete channel functionality

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -9,6 +9,8 @@ import MembersModal from "@/components/modals/members-modal";
 import CreateChannelModal from "@/components/modals/create-channel-modal";
 import LeaveServerModal from "@/components/modals/leave-server-modal";
 import DeleteServerModal from "@/components/modals/delete-server-modal";
+import DeleteChannelModal from "@/components/modals/delete-channel-modal";
+import EditChannelModal from "@/components/modals/edit-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState<boolean>(false);
@@ -28,6 +30,8 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <DeleteServerModal />
       <LeaveServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
